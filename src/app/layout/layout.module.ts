8ffa2874e3907192d0a9400a290229e184f20d06
Shittu Temitayo <union@Emperors-MBP.lan>
2022-11-15 22:18:08 +0100

    Addition oG

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BaseComponent } from './base/base.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BaseComponent
   ],
   imports: [CommonModule],
 })
