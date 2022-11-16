fc677cc139764bbf240b59decb303b1a29e4f7f1
Shittu Temitayo <union@Emperors-MBP.lan>
2022-11-16 18:57:00 +0100

    Delete pipe Test File

--- src/app/core/pipe/currency-converter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CurrencyConverterPipe } from './currency-converter.pipe';
-
-describe('CurrencyConverterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CurrencyConverterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
